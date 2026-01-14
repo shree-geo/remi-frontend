fc1eca913899b8b1098bd3e45a19a32b73744864
saheel <programmingwithsahil@gmail.com>
2026-01-14 11:14:17 +0545

    fix: made size optional and default p element is size

--- components/molecules/text/index.tsx
+++ components/molecules/text/index.tsx
@@ -1,17 +1,18 @@
 import { cn } from "@/lib/utils";
 import type { HTMLAttributes, PropsWithChildren } from "react";
 
+type TextSize = "p" | "lead" | "large" | "small";
 interface TextPropsBase
   extends Omit<HTMLAttributes<HTMLParagraphElement | HTMLSpanElement>, "slot"> {
   slot?: "span" | "p";
-  size: "p" | "lead" | "large" | "small";
+  size?: TextSize;
 }
 type TextProps = PropsWithChildren<TextPropsBase>;
 
 export default function Text(props: TextProps) {
-  const { slot, size, className = "", ...rest } = props;
+  const { slot, size = "p", className = "", ...rest } = props;
   const Component = slot || "p";
-  const variantClass: Record<TextProps["size"], string> = {
+  const variantClass: Record<TextSize, string> = {
     p: "leading-7 [&:not(:first-child)]:mt-6",
     lead: "text-muted-foreground text-xl",
     large: "text-lg font-semibold",
