ee384699ca9f3e4e2d1d1ace861c427d58dfb8d1
Kumar Adhikari <kumarad556@gmail.com>
2026-01-14 10:23:35 +0545

    fix: changed translation component to server

--- components/molecules/Form/textArea.tsx
+++ components/molecules/Form/textArea.tsx
@@ -8,7 +8,7 @@ import { Label } from "@/components/ui/label";
 import { TranslationDefinition } from "@/definitions/translation.definition";
 import { AlertCircleIcon, InfoIcon } from "lucide-react";
 import { ComponentProps } from "react";
-import CTranslation from "../translations/CTranslation";
+import STranslation from "../translations/STranslation";
 
 interface InputElementProps extends ComponentProps<typeof InputGroupTextarea> {
   label: TranslationDefinition;
@@ -24,7 +24,7 @@ export default function TextAreaElement(props: InputElementProps) {
         <InputGroupTextarea {...rest} />
         <InputGroupAddon align="block-start">
           <Label>
-            <CTranslation {...label} />
+            <STranslation {...label} />
             {required && (
               <InputGroupText className="text-rose-600">*</InputGroupText>
             )}
@@ -35,14 +35,14 @@ export default function TextAreaElement(props: InputElementProps) {
       {error && (
         <div className="flex items-center space-x-2 px-2 pt-1 text-rose-600">
           <AlertCircleIcon className="h-4" />
-          {typeof error === "object" ? <CTranslation {...error} /> : error}
+          {typeof error === "object" ? <STranslation {...error} /> : error}
         </div>
       )}
 
       {helper && (
         <div className="flex items-center space-x-2 px-2 pt-1">
           <InfoIcon className="h-4" />
-          {typeof helper === "object" ? <CTranslation {...helper} /> : helper}
+          {typeof helper === "object" ? <STranslation {...helper} /> : helper}
         </div>
       )}
     </div>
