3edf85adf515f819c4cb164ef8aec2831f589316
sanjiv029 <ss164761@gmail.com>
2026-01-15 15:07:29 +0545

    fixes: made user form fully client component

--- modules/settings/users/components/formWrapper.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import { PropsWithChildren, useActionState } from "react";
-import createUserAction from "../actions/createUserAction";
-
-export default function UserFormWrapper({ children }: PropsWithChildren) {
-  const [, action] = useActionState(createUserAction, undefined);
-  return <form action={action}>{children}</form>;
-}
--- modules/settings/users/definitions/user.definitions.ts
+++ modules/settings/users/definitions/user.definitions.ts
@@ -1,7 +1,11 @@
 import z from "zod";
 export const userSchema = z.object({
   email: z.string().email(),
-  full_name: z.string().min(3),
+  first_name: z
+    .string()
+    .min(3, "First name must be at least 3 characters long"),
+  last_name: z.string().min(3, "Last name must be at least 3 characters long"),
+  employee_id: z.string().optional(),
   role: z.enum(["admin", "user"]),
-  password: z.string().min(8),
+  password: z.string().min(8, "Password must be at least 8 characters long"),
 });
